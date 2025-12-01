234f7629eea9c5e13dfc324db67f412dcf919eb7
ColtonWright55 <wright.2135@buckeyemail.osu.edu>
2025-12-01 00:46:11 -0500

    b-spline p2g

--- mpm_forge/engine/solvers/ulmpm_solver.py
+++ mpm_forge/engine/solvers/ulmpm_solver.py
@@ -37,63 +37,150 @@ def reset_grid(grid_m: wp.array4d(dtype=float), grid_mv: wp.array4d(dtype=wp.vec
 @wp.kernel
 def p2g(
     # Input parameters
-    p_x: wp.array2d(dtype=wp.vec2),
-    p_v: wp.array2d(dtype=wp.vec2),
-    p_mass: wp.array2d(dtype=float),
-    p_stress: wp.array(dtype=wp.mat33),
-    p_vol: wp.array(dtype=float),
-    grid_mass: wp.array(dtype=float),
-    grid_mom: wp.array(dtype=wp.vec3),
-    grid_force: wp.array(dtype=wp.vec3),
-    # dx: float,
-    # inv_dx: float,
-):
-    """
-    Standard particle to grid. Updated Lagrangian specific. This is not what is in Genesis (but similar).
-    """
-    p = wp.tid()
-
-
-@wp.kernel
-def update_grid(
+    p_x: wp.array2d(dtype=wp.vec3),
+    p_v: wp.array2d(dtype=wp.vec3),
+    p_m: wp.array2d(dtype=float),
+    p_vol: wp.array2d(dtype=float),
+    p_stress: wp.array2d(dtype=wp.mat33),
     grid_m: wp.array4d(dtype=float),
     grid_mv: wp.array4d(dtype=wp.vec3),
     grid_f: wp.array4d(dtype=wp.vec3),
     params: DeviceParams,
-    grid_v_next: wp.array4d(dtype=wp.vec3),
 ):
-    p = wp.tid()
+    """
+    Standard particle to grid. Updated Lagrangian specific. This is not what is in Genesis (but similar).
+
+    See [https://doi.org/10.1007/978-3-031-24070-6] Section 2.5 Algorithm 1
+
+    TODO: Obviously there is some stuff in there that can be optimized, if statements etc.
+    """
+    tid = wp.tid()
+    n_particles = p_x.shape[1]
+    b = tid // n_particles  # Batch index
+    p = tid % n_particles  # Particle index within batch
+    xp = p_x[b, p]
+    vp = p_v[b, p]
+    mp = p_m[b, p]
+    vol = p_vol[b, p]
+    stress = p_stress[b, p]
+
+    # Figure out what grid particle is in
+    base_pos = xp * params.inv_dx - wp.vec3(1.0)
+    v_fx = wp.floor(base_pos[0])
+    v_fy = wp.floor(base_pos[1])
+    v_fz = wp.floor(base_pos[2])
+    base_idx_x = int(v_fx)
+    base_idx_y = int(v_fy)
+    base_idx_z = int(v_fz)
+    grid_pos_floor = wp.vec3(v_fx, v_fy, v_fz)
+    fx = base_pos - grid_pos_floor
+
+    # Cubic weights
+    wx = cubic_weights(fx[0])
+    wy = cubic_weights(fx[1])
+    wz = cubic_weights(fx[2])
+    dwx = cubic_grad_weights(fx[0], params.inv_dx)
+    dwy = cubic_grad_weights(fx[1], params.inv_dx)
+    dwz = cubic_grad_weights(fx[2], params.inv_dx)
+
+    # We are using cubic B-splines, and therefore in each direction there are 4 non-zero basis functions. We must add
+    # this particles contribution to all its neighboring grid nodes.
+    for i in range(4):
+        for j in range(4):
+            for k in range(4):
+                ix = base_idx_x + i
+                iy = base_idx_y + j
+                iz = base_idx_z + k
+
+                # Tensor Product Weight
+                w_val = wx[i] * wy[j] * wz[k]
+
+                # Grad (Chain Rule)
+                grad_val = wp.vec3(dwx[i] * wy[j] * wz[k], wx[i] * dwy[j] * wz[k], wx[i] * wy[j] * dwz[k])
+
+                # Compute nodal mass
+                mass_contrib = mp * w_val
+                # wp.atomic_add(grid_m, (b, ix, iy, iz), mass_contrib)
+
+                # Compute nodal momentum
+                mom_contrib = mass_contrib * vp
+                if params.is_2d == 1:
+                    mom_contrib = wp.vec3(mom_contrib[0], mom_contrib[1], 0.0)
+                # wp.atomic_add(grid_mv, (b, ix, iy, iz), mom_contrib)
+
+                # Compute external force, internal force, nodal force
+                # f_ext_contrib = 0.0 # You could add gravity in here, uninteresting
+                # f_int_contrib = -1.0 * vol * (stress * grad_val)
+                # f_total_contrib = f_ext_contrib + f_int_contrib
+                # wp.atomic_add(grid_f,  (b, ix, iy, iz), f_total_contrib)
+                f_contrib = -1.0 * vol * (stress * grad_val)
+                if params.is_2d == 1:
+                    f_contrib = wp.vec3(f_contrib[0], f_contrib[1], 0.0)
+                # wp.atomic_add(grid_f, (b, ix, iy, iz), f_contrib)
+
+    if params.is_2d == 1:
+        pass
 
 
-@wp.kernel
-def g2p(
-    grid_v_next: wp.array4d(dtype=wp.vec3),
-    params: DeviceParams,
-    p_x: wp.array2d(dtype=wp.vec3),
-    p_v: wp.array2d(dtype=wp.vec3),
-    p_C: wp.array2d(dtype=wp.mat33),
-    p_F: wp.array2d(dtype=wp.mat33),
-    p_F_plastic: wp.array2d(dtype=wp.mat33),
-    p_stress: wp.array2d(dtype=wp.mat33),
-    p_vol: wp.array2d(dtype=float),
-):
-    p = wp.tid()
+@wp.func
+def cubic_weights(x: float):
+    """
+    Returns the 4 weights for a cubic B-spline at relative distance x.
+    x is the fraction (0.0 to 1.0) of the particle position relative to the cell.
+
+    See [https://doi.org/10.1007/978-3-031-24070-6] Section  3.2
+    """
+
+    # Precompute powers
+    x2 = x * x
+    x3 = x2 * x
+
+    w0 = (1.0 / 6.0) * (1.0 - x) * (1.0 - x) * (1.0 - x)
+    w1 = (1.0 / 6.0) * (3.0 * x3 - 6.0 * x2 + 4.0)
+    w2 = (1.0 / 6.0) * (-3.0 * x3 + 3.0 * x2 + 3.0 * x + 1.0)
+    w3 = (1.0 / 6.0) * x3
+
+    return wp.vec4(w0, w1, w2, w3)
 
 
-# while t < t_f:
-#     # 1. Reset grid (launch with grid dimensions)
-#     wp.launch(kernel=reset_grid, dim=grid_res)
+@wp.func
+def cubic_grad_weights(x: float, inv_dx: float):
+    """
+    Returns the 4 gradients (dW/dx) for a cubic B-spline.
+    """
+    x2 = x * x
 
-#     # 2. P2G (launch with particle count)
-#     wp.launch(kernel=p2g, dim=n_particles, inputs=[...])
+    dw0 = (1.0 / 6.0) * (-3.0 * (1.0 - x) * (1.0 - x))
+    dw1 = (1.0 / 6.0) * (9.0 * x2 - 12.0 * x)
+    dw2 = (1.0 / 6.0) * (-9.0 * x2 + 6.0 * x + 3.0)
+    dw3 = (1.0 / 6.0) * (3.0 * x2)
 
-#     # 3. Update grid (launch with grid dimensions)
-#     # wp.launch(kernel=update_grid, dim=grid_res, inputs=[dt])
+    return wp.vec4(dw0, dw1, dw2, dw3) * inv_dx
 
-#     # 4. G2P (launch with particle count)
-#     # wp.launch(kernel=g2p, dim=n_particles, inputs=[dt])
 
-#     t = t + dt
+# @wp.kernel
+# def update_grid(
+#     grid_m: wp.array4d(dtype=float),
+#     grid_mv: wp.array4d(dtype=wp.vec3),
+#     grid_f: wp.array4d(dtype=wp.vec3),
+#     params: DeviceParams,
+#     grid_v_next: wp.array4d(dtype=wp.vec3),
+# ):
+#     p = wp.tid()
+
+
+# @wp.kernel
+# def g2p(
+#     grid_v_next: wp.array4d(dtype=wp.vec3),
+#     params: DeviceParams,
+#     p_x: wp.array2d(dtype=wp.vec3),
+#     p_v: wp.array2d(dtype=wp.vec3),
+#     p_C: wp.array2d(dtype=wp.mat33),
+#     p_F: wp.array2d(dtype=wp.mat33),
+#     p_stress: wp.array2d(dtype=wp.mat33),
+#     p_vol: wp.array2d(dtype=float),
+# ):
+#     p = wp.tid()
 
 
 class MPMULSolver:
@@ -136,8 +223,8 @@ class MPMULSolver:
         # Prepare Device Params
         self.params = DeviceParams()
         self.params.dt = config.dt
-        # self.params.dx = config.dx # TODO: ?
-        # self.params.inv_dx = 1.0 / config.dx
+        self.params.dx = 1 / (self.grid_shape[1] - 1)
+        self.params.inv_dx = 1.0 / self.params.dx
         # self.params.alpha = config.pic_flip_alpha
         self.params.E = config.E
         self.params.nu = config.nu
@@ -152,11 +239,11 @@ class MPMULSolver:
         Advance by one explicit time step
         """
         B = self._n_envs
-        N = self._n_particles
-        H, W = self._grid_res
+        # N = self._n_particles
+        _, H, W = self._grid_res
 
         # 1. Reset grid
-        wp.launch(kernel=reset_grid, dim=(B, H, W), inputs=[self.grid_m, self.grid_v, self.grid_f])
+        wp.launch(kernel=reset_grid, dim=(B, H, W), inputs=[self.grid_m, self.grid_mv, self.grid_f])
 
         # 2. P2G
         wp.launch(
@@ -167,37 +254,36 @@ class MPMULSolver:
                 self.p_v,
                 self.p_m,
                 self.p_vol,
-                self.p_C,
                 self.p_stress,
-                self.params,
                 self.grid_m,
                 self.grid_mv,
                 self.grid_f,
+                self.params,
             ],
         )
 
         # 3. Update grid / apply BCs
-        wp.launch(
-            kernel=update_grid,
-            dim=self.grid_shape,
-            inputs=[self.grid_m, self.grid_mv, self.grid_f, self.params, self.grid_v_next],
-        )
+        # wp.launch(
+        #     kernel=update_grid,
+        #     dim=self.grid_shape,
+        #     inputs=[self.grid_m, self.grid_mv, self.grid_f, self.params, self.grid_v_next],
+        # )
 
         # 4. G2P
-        wp.launch(
-            kernel=g2p,
-            dim=(self.B, self.N),
-            inputs=[
-                self.grid_v_next,
-                self.params,
-                self.p_x,
-                self.p_v,
-                self.p_C,
-                self.p_F,
-                self.p_F_p,
-                self.p_stress,
-                self.p_vol,
-            ],
-        )
+        # wp.launch(
+        #     kernel=g2p,
+        #     dim=(self.B, self.N),
+        #     inputs=[
+        #         self.grid_v_next,
+        #         self.params,
+        #         self.p_x,
+        #         self.p_v,
+        #         self.p_C,
+        #         self.p_F,
+        #         self.p_F_p,
+        #         self.p_stress,
+        #         self.p_vol,
+        #     ],
+        # )
         # End
         self.t += self.config.dt
